469c503505a7b86e6b3287617811d2f0e4ba1daf
dstarrtrey <dstarrtrey@gmail.com>
2019-03-13 14:19:04 -0700

    Adds logic for apiRoutes.js match and POST request in survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,6 +4,25 @@ const apiRoutes = app => {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+  app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+    console.log("new friend: ", newFriend);
+    const differences = [];
+    friends.forEach(item => {
+      console.log(item.name);
+      differences.push(
+        item.scores
+          .map((x, i) => {
+            return Math.abs(x - newFriend.scores[i]);
+          })
+          .reduce((x, y) => x + y)
+      );
+    });
+    const match = differences.indexOf(Math.min(...differences));
+    console.log(differences, match);
+    friends.push(newFriend);
+    res.json(friends[match]);
+  });
 };
 
 module.exports = apiRoutes;
